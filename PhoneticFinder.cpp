e354c86fc99ac322f210486fc8a704f740a47610
NaomiO <naomioyer95@gmail.com>
2020-03-22 14:16:55 +0200

    "add 3 new files"

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdexcept>
+#include "PhoneticFinder.hpp"
+
+using namespace std;
+
+string find( string text, const char[]){
+    cout << "Hi" << endl;
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+namespace phonetic{
+
+    string find(string text, const char[]);
+    
+};
