46de505a1dd303b33910dcb5f2ce60a56854554d
NaomiO <naomioyer95@gmail.com>
2020-03-23 19:17:28 +0200

    commit

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -3,6 +3,7 @@ using namespace std;
 
 namespace phonetic{
 
-    string find(string text, const char word[]);
-    
+    string find(string text, string word);
+    int compareWord(string tempWord, string word);
+
 };
